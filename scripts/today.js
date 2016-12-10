5aa181a968b662941661ffdefb1a16538f441eab
Austin Kemper <austin.kemper@live.com>
2016-12-09 22:09:05 -0500

    created today.js

--- scripts/today.js
+++ scripts/today.js
@@ -3,10 +3,28 @@ window.onload = init;
 function init(){
 	setUpHHPC();
 	setDate();
+	startTime();
 }
 
 var hhpc;
 
+
+function startTime() {
+    var today = new Date();
+    var h = today.getUTCHours();
+    var m = today.getUTCMinutes();
+    var s = today.getUTCSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var timeSlot = document.getElementById("timeSlot");
+    timeSlot.innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 
 function createMonth(name,numDays,index){
 	var month = {
@@ -103,7 +121,6 @@ function setUpHHPC(){
 
 function setDate(){
 	var dateElement = document.getElementById("todaysdate");
-	var welcomeString = "Today's HHPC Date is: <br>"
-	dateElement.innerHTML = welcomeString + hhpc.toStringYearFirst();
+	dateElement.innerHTML = hhpc.toStringYearFirst();
 }
 
